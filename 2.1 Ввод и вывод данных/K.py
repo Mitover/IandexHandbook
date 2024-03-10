6ee8e6a0ae7e901ee6c4c6c10da7bb93460601ad
Mitover <76473397+Mitover@users.noreply.github.com>
2024-03-10 11:18:30 +0500

    add solution

--- 2.1 Ввод и вывод данных/K.py
+++ 2.1 Ввод и вывод данных/K.py
@@ -1,2 +1,5 @@
 number = int(input())
 print(str(number % 1000 // 100) + str(number // 1000) + str(number % 10) + str(number // 10 % 10))
+
+number = input()
+print(number[1] + number[0] + number[3] + number[2])
--- 2.3 Циклы/H.py
+++ 2.3 Циклы/H.py
@@ -1,6 +1,16 @@
-i = 0
+# i = 0
+# word = input()
+# n = int(input())
+# while i < n:
+#     print(word)
+#     i += 1
+#------------------
 word = input()
 n = int(input())
-while i < n:
+for i in range(n):
     print(word)
-    i += 1
+#------------------
+word = input()
+n = int(input())
+words = (word + "\n") * n
+print(words)
