3128fbc37a727eee97db5c1a179165169fccd6d5
Mitover <76473397+Mitover@users.noreply.github.com>
2024-07-26 09:08:52 +0500

    add solution

---
+++ 3.3 Списочные вырадения. Модель памяти для типов языка Python/D.py
@@ -0,0 +1,1 @@
+{i for i in set(numbers)if i%2 != 0}
--- 4.2. Позиционные и именованные аргументы. Функции высших порядков. Лямбда-функции/B.py
+++ 4.2. Позиционные и именованные аргументы. Функции высших порядков. Лямбда-функции/B.py
@@ -1,4 +1,30 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+def make_matrix(size, value=0):
+    if type(size) is int:
+        return [[value for j in range(size)] for i in range(size)]
+    else:
+        return [[value for j in range(size[0])] for i in range(size[1])]
+###
+def make_matrix(size, value=0):
+    if type(size) == int:
+        return [[value for j in range(size)] for i in range(size)]
+    else:
+        return [[value for j in range(size[0])] for i in range(size[1])]
+###   
+def make_matrix(size, value=0):
+    if isinstance(size, int):
+        return [[value for j in range(size)] for i in range(size)]
+    else:
+        return [[value for j in range(size[0])] for i in range(size[1])]
+###
+def make_matrix(size, value=0):
+    list_numbers = []
+    if type(size) == int:
+        for _ in range(size):
+            for _ in range(size):
+                list_numbers.append(value)
+        return list_numbers
+    else:
+        for _ in range(size[0]):
+            for _ in range(size[1]):
+                list_numbers.append(value)
+        return list_numbers
