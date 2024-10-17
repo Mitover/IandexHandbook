ec3b436fd9b9223c4903c9d605edc4f9f3a9134a
Mitover <76473397+Mitover@users.noreply.github.com>
2024-10-17 16:56:50 +0500

    edit

--- 2.3 Циклы/C.py
+++ 2.3 Циклы/C.py
@@ -1,9 +1,8 @@
-# a = int(input())
-# b = int(input())
-# for i in range(a, b + 1):
-#     print(i, end=" ")
-
-
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    print(i, end=" ")
+###
 a = int(input())
 b = int(input())
 while a <= b:
