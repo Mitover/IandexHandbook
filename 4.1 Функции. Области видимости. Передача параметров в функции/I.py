ac3fcead65113a7c374a972c8acd9cb4f3dd7f81
Mitover <76473397+Mitover@users.noreply.github.com>
2025-02-25 12:12:19 +0500

    add

--- 4.1 Функции. Области видимости. Передача параметров в функции/I.py
+++ 4.1 Функции. Области видимости. Передача параметров в функции/I.py
@@ -8,3 +8,12 @@ def is_prime(number):
             return False
         divider += 1
     return True
+#####
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for div in range(2, int(number**0.5)):
+        if number % div == 0:
+            return False
+    return True
